1052ca06114e6dacb8c01ff5335e671f1e992e1a
Sergey Eliseev <s.eliseev1@gmail.com>
2024-01-03 20:03:40 +0300

    3. Тестовое задание - проект "Pooh JMS" [#268841]

---
+++ src/main/java/ru/job4j/pooh/TopicSchema.java
@@ -0,0 +1,50 @@
+package ru.job4j.pooh;
+
+import java.util.Iterator;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class TopicSchema implements Schema {
+    private final ConcurrentHashMap<String, CopyOnWriteArrayList<Receiver>> receivers = new ConcurrentHashMap<>();
+    private final ConcurrentHashMap<String, BlockingQueue<String>> data = new ConcurrentHashMap<>();
+    private final Condition condition = new Condition();
+
+    @Override
+    public void addReceiver(Receiver receiver) {
+        receivers.putIfAbsent(receiver.name(), new CopyOnWriteArrayList<>());
+        receivers.get(receiver.name()).add(receiver);
+        condition.on();
+    }
+
+    @Override
+    public void publish(Message message) {
+        data.putIfAbsent(message.name(), new LinkedBlockingQueue<>());
+        data.get(message.name()).add(message.text());
+        condition.on();
+    }
+
+    @Override
+    public void run() {
+        while (!Thread.currentThread().isInterrupted()) {
+            for (String topic : receivers.keySet()) {
+               BlockingQueue<String> msgs = data.getOrDefault(topic, new LinkedBlockingQueue<>());
+                while (!msgs.isEmpty()) {
+                    String message = msgs.poll();
+                    Iterator<Receiver> receiver  = receivers.get(topic).iterator();
+                    while (receiver.hasNext()) {
+                        Receiver rcv = receiver.next();
+                        rcv.receive(message);
+                    }
+                }
+            }
+            condition.off();
+            try {
+                condition.await();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
+}
